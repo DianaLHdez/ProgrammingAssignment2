af91111a5ed8abd4731fc0134a50046d20cabefc
DianaLHdez <dianalhdez@hotmail.com>
2017-04-23 22:33:00 -0500

    Add cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function helps making a square matrix with random integers
+## you could give the dimmensions if not default value is 3x3
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(), dimmensions=3) {
+	a<-dimmensions
+	c<-a*a
+	n<-100
+	d<-sample(n,c)
+	e<-matrix(d,a,a)
+	f<-solve(e)
+	list(matrix=e, solve=f)
 }
 
 
-## Write a short comment describing this function
+## This function look for the inverse of a matrix given if not inverse ##the matrix, first looks for a list and after that looks for solve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+	if(is.list(x)){
+		if(is.null(x$solve)){
+			h<-solve(x)
+		} else {
+		print(x$solve)}}
+	else{
+		h<-solve(x)
+}}
